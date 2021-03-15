57fb49d3c099bcb019fc98bc3803b2d5e151ef11
DenisBukva <denisbukva@hotmail.com>
2021-03-15 12:39:54 +0100

    add ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,8 @@
+function xSix(num) {
+    return num * 6
+}
+console.log(xSix(5)); var answer = xSix(5);
+console.log(xSix(2)); var answer = xSix(2);
+console.log(xSix(0)); var answer = xSix(0);
+
+module.exports = xSix;
